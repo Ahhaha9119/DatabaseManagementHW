33792a5d0b5e729063857883fd089b8cabdc9ab9
WAhhaha <skyblocksians@gmail.com>
2023-10-19 17:24:27 +0800

    tidy up with folder.

---
+++ scripts/insertion.sql
@@ -0,0 +1,47 @@
+INSERT INTO 商品 (品號, 品名, 單位) VALUES (
+  N'A01',
+  N'超級登山車 26*18吋',
+  N'台'
+);
+
+INSERT INTO 商品 (品號, 品名, 單位) VALUES (
+  N'A02',
+  N'越野登山車 26*18吋',
+  N'台'
+);
+
+INSERT INTO 商品 (品號, 品名, 單位) VALUES (
+  N'',
+  N'',
+  N''
+);
+INSERT INTO 商品 (品號, 品名, 單位) VALUES (
+  N'',
+  N'',
+  N''
+);
+INSERT INTO 商品 (品號, 品名, 單位) VALUES (
+  N'',
+  N'',
+  N''
+);
+INSERT INTO 商品 (品號, 品名, 單位) VALUES (
+  N'',
+  N'',
+  N''
+);
+INSERT INTO 商品 (品號, 品名, 單位) VALUES (
+  N'',
+  N'',
+  N''
+);
+INSERT INTO 商品 (品號, 品名, 單位) VALUES (
+  N'',
+  N'',
+  N''
+);
+INSERT INTO 商品 (品號, 品名, 單位) VALUES (
+  N'',
+  N'',
+  N''
+);
